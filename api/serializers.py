5e37c25306b9c008ff33733e4b344dce36fb3bd3
Daniel olascoaga <dan.j.olascoaga.f@gmail.com>
2023-06-09 15:48:42 -0500

    Adding serializers

---
+++ api/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import Risk, Worker, Company
+
+class RiskSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Risk
+    fields = ['id', 'enviroment', 'levelRisk', 'probability', 'gravity', 'description', 'actions']
+
+class WorkerSerializer(serializers.ModelSerializer):
+  risks = serializers.SerializerMethodField()
+
+  class Meta:
+    model = Worker
+    fields = ['id','name', 'lastname', 'age', 'sex', 'email', 'phone', 'address', 'profession', 'risks', 'roleInCompany', 'created_at']
+
+    def get_risks(self, obj):
+      risks = obj.risks.all()
+      return RiskSerializer(risks, many=True).data 
+
+class CompanySerializer(serializers.ModelSerializer):
+  workers = WorkerSerializer()
+
+  class Meta:
+    model = Company
+    fields = ['id', 'nit', 'name', 'email', 'phone', 'address', 'password', 'risks', 'created_at'] 
