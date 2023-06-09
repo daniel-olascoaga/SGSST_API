9b24a532888885506e16cf1d77049df296e71722
Daniel olascoaga <dan.j.olascoaga.f@gmail.com>
2023-06-09 15:57:38 -0500

    Adding views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Risk, Worker, Company
+from .serializers import RiskSerializer, WorkerSerializer, CompanySerializer
 
 # Create your views here.
+class RiskViewSet(viewsets.ModelViewSet):
+  queryset = Risk.objects.all()
+  serializers_class = RiskSerializer
+
+class WorkerViewSet(viewsets.ModelViewSet):
+  queryset = Worker.objects.all()
+  serializers_class = WorkerSerializer
+
+class CompanyViewset(viewsets.ModelViewSet):
+  queryset = Company.objects.all()
+  serializers_class = CompanySerializer
